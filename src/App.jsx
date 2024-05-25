79d85fe100a12b42c129cea6156d6d0921fa2840
mdshimul101 <shimulfjr101@gmail.com>
2024-05-26 00:03:32 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 className="bg-slate-500 text-3xl">Home</h1>
+      <button className="btn">Button</button>
+    </div>
+  );
+}
+
+export default App;
